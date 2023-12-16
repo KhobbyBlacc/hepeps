c5443a122ebb590d8d51dbf534147829ba1ba0ef
Khobby Amihere <khobbyamihere@MeatPie-2.local>
2023-12-16 20:10:08 +0000

    Commit

--- lib/sockets/connection.dart
+++ lib/sockets/connection.dart
@@ -1,8 +1,9 @@
+import 'package:chatheeps/util/constants.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:web_socket_channel/status.dart' as status;
 
 connection() async {
-  final socket = Uri.parse('ws://your-backend-url');
+  final socket = Uri.parse(SOCKET_URL);
   var channel = WebSocketChannel.connect(socket);
 
   channel.stream.listen((message) {
